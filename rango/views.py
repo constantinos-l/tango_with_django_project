ca1c78a8b02a5e4db73ace1ea8268d0ad2453a29
“Constantinos <2099999z@student.gla.ac.uk>
2022-02-04 18:33:42 +0000

    Completed some of Chapter 8's exercises

--- rango/views.py
+++ rango/views.py
@@ -17,8 +17,6 @@ def index(request):
 	return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-	#return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
-	#context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
 	return render(request, 'rango/about.html')
 
 def show_category(request, category_name_slug):
